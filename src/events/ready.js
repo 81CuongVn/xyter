f138ceaa2278abbf06d7825a53fa9b8a43afca5c
Axel Olausson Holtenäs <vermium@zyner.org>
2022-03-06 12:44:34 +0100

    ✨ bot status

--- src/events/ready.js
+++ src/events/ready.js
@@ -5,5 +5,6 @@ module.exports = {
   once: true,
   async execute(client) {
     await logger.info(`Ready! Logged in as ${client.user.tag}`);
+    await client.user.setPresence({ activities: [{ type: 'WATCHING', name: `${client.guilds.cache.size} guilds` }], status: 'online' });
   },
 };
