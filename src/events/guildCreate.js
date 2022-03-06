f47bdde051f1288d4336401372ce64c42f967eaf
Axel Olausson Holtenäs <vermium@zyner.org>
2022-03-06 14:18:22 +0100

    🎨 adding guild at join

---
+++ src/events/guildCreate.js
@@ -0,0 +1,15 @@
+const config = require('../../config.json');
+const logger = require('../handlers/logger');
+
+const guilds = require('../helpers/database/models/guildSchema');
+
+module.exports = {
+  name: 'interactionCreate',
+  async execute(guild) {
+    const guildExist = await guilds.findOne({ guildId: guild.id });
+
+    if (!guildExist) { await guilds.create({ guildId: guild.id }); }
+
+    console.log(guildExist);
+  },
+};
