5054e6d3ca64af4f66ef5ad079a441ccee4ed773
Axel Olausson Holtenäs <vermium@zyner.org>
2022-03-09 22:14:25 +0100

    ✨ live update watching guilds

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -3,10 +3,12 @@ const guilds = require('../helpers/database/models/guildSchema');
 module.exports = {
   name: 'guildCreate',
   async execute(guild) {
+    const { client } = guild;
     await guilds
       .findOne(
         { guildId: guild.id },
         { new: true, upsert: true },
       );
+    await client.user.setPresence({ activities: [{ type: 'WATCHING', name: `${client.guilds.cache.size} guilds` }], status: 'online' });
   },
 };
