6a85d20a037cc4970762d83fbc8f5a279a546622
Axel Olausson Holtenäs <vermium@zyner.org>
2022-03-14 23:08:43 +0100

    🩹 fix api config

--- src/dashboard/index.js
+++ src/dashboard/index.js
@@ -218,7 +218,7 @@ module.exports = async (client) => {
             optionType: DBD.formTypes.input('https://bg.zyner.org/api/'),
             getActualSet: async ({ guild, user }) => {
               const api = await apis.findOne({ guildId: guild.id });
-              return api.url || null;
+              return api ? api.url : null;
             },
             setNew: async ({ guild, newData }) => {
               const api = await apis.findOne({ guildId: guild.id });
@@ -237,7 +237,7 @@ module.exports = async (client) => {
             optionType: DBD.formTypes.input('1'),
             getActualSet: async ({ guild, user }) => {
               const api = await apis.findOne({ guildId: guild.id });
-              return api.token || null;
+              return api.token ? api.token : null;
             },
             setNew: async ({ guild, newData }) => {
               const api = await apis.findOne({ guildId: guild.id });
