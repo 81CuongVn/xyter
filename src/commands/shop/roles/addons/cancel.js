c9c245df1fa9de27410c86db6e2720c953e003c8
Vermium Sifell <vermium@zyner.org>
2022-03-17 11:33:24 +0100

    Update cancel.js

--- src/commands/shop/roles/addons/cancel.js
+++ src/commands/shop/roles/addons/cancel.js
@@ -54,7 +54,7 @@ module.exports = async (interaction) => {
           timestamp: new Date(),
           footer: { iconURL: config.footer.icon, text: config.footer.text },
         };
-        return await interaction.editReply({
+        return interaction.editReply({
           embeds: [embed],
           ephemeral: true,
         });
