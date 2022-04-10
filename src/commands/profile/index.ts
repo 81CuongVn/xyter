407603540e756068060f675798b6bd34d921b4d1
codefactor-io <support@codefactor.io>
2022-04-10 01:18:47 +0000

    [CodeFactor] Apply fixes to commit eb1aa6c

--- src/commands/profile/index.ts
+++ src/commands/profile/index.ts
@@ -1,28 +1,28 @@
 // Dependencies
-import { SlashCommandBuilder } from '@discordjs/builders';
-import { CommandInteraction } from 'discord.js';
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { CommandInteraction } from "discord.js";
 
 // Modules
-import view from './modules/view';
+import view from "./modules/view";
 
 // Function
 export default {
   data: new SlashCommandBuilder()
-    .setName('profile')
-    .setDescription('Check a profile.')
+    .setName("profile")
+    .setDescription("Check a profile.")
     .addSubcommand((subcommand) =>
       subcommand
-        .setName('view')
-        .setDescription('View a profile.')
+        .setName("view")
+        .setDescription("View a profile.")
         .addUserOption((option) =>
           option
-            .setName('target')
-            .setDescription('The profile you wish to view')
+            .setName("target")
+            .setDescription("The profile you wish to view")
         )
     ),
   async execute(interaction: CommandInteraction) {
     // Module - View
-    if (interaction.options.getSubcommand() === 'view') {
+    if (interaction.options.getSubcommand() === "view") {
       // Execute Module - View
       await view(interaction);
     }
