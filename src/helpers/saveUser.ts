c2260332b48a55ad98ef403adcb5c04210a02a36
Vermium Sifell <vermium@zyner.org>
2022-04-11 22:06:33 +0200

    🔒️ replaced Math.random() with chance.integer()

--- src/helpers/saveUser.ts
+++ src/helpers/saveUser.ts
@@ -1,28 +1,39 @@
-import sleep from './sleep';
-import logger from '../handlers/logger';
+import sleep from "./sleep";
+import logger from "../handlers/logger";
+import Chance from "chance";
 
 export default async function saveUser(data: any, data2: any) {
   process.nextTick(
     async () => {
-      await sleep(Math.floor(Math.random() * 10 + 1) * 100); // 100 - 1000 random  Number generator
+      // Chance module
+      const chance = new Chance();
+
+      await sleep(
+        chance.integer({
+          min: 0,
+          max: 1,
+        }) *
+          10 +
+          1 * 100
+      ); // 100 - 1000 random  Number generator
       data.save((_: any) =>
         _
           ? logger.error(
-              `ERROR Occurred while saving data (saveUser) \n${'='.repeat(
+              `ERROR Occurred while saving data (saveUser) \n${"=".repeat(
                 50
-              )}\n${`${_}\n${'='.repeat(50)}`}`
+              )}\n${`${_}\n${"=".repeat(50)}`}`
             )
-          : 'No Error'
+          : "No Error"
       );
       if (data2) {
         data2.save((_: any) =>
           _
             ? logger.error(
-                `ERROR Occurred while saving data (saveUser) \n${'='.repeat(
+                `ERROR Occurred while saving data (saveUser) \n${"=".repeat(
                   50
-                )}\n${`${_}\n${'='.repeat(50)}`}`
+                )}\n${`${_}\n${"=".repeat(50)}`}`
               )
-            : 'No Error'
+            : "No Error"
         );
       }
     },
