e3cc352fc2f41bc762fb18f65e2621fa8fbd89b1
Vermium Sifell <vermium@zyner.org>
2022-04-12 22:41:22 +0200

    🥅 guild is null

--- src/handlers/schedules.ts
+++ src/handlers/schedules.ts
@@ -25,6 +25,7 @@ export default async (client: Client) => {
             guildId: shopRole.guildId,
           });
 
+          if (guild === null) return;
           const userDB = await users.findOne({
             userId: shopRole.userId,
             guildId: shopRole.guildId,
