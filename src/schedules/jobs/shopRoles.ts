a2e5f1545ad7d7c15256783adeb5e6edd54f6b4d
Vermium Sifell <vermium@zyner.org>
2022-04-17 23:05:13 +0200

    ⚡️ remove shop roles if rMember or rRole not found

--- src/schedules/jobs/shopRoles.ts
+++ src/schedules/jobs/shopRoles.ts
@@ -57,11 +57,45 @@ export default async (client: Client) => {
 
       if (!rRole) {
         logger.error(`Role ${roleId} not found for shop role ${roleId}.`);
+        await shopRoleSchema
+          .deleteOne({
+            userId,
+            roleId,
+            guildId,
+          })
+          .then(async () => {
+            logger.verbose(
+              `Shop role document ${roleId} has been deleted from user ${userId}.`
+            );
+          })
+          .catch(async (error) => {
+            logger.error(
+              `Error deleting shop role document ${roleId} from user ${userId}.`,
+              error
+            );
+          });
         return;
       }
 
       if (!rMember) {
         logger.error(`Member ${userId} not found for shop role ${roleId}.`);
+        await shopRoleSchema
+          .deleteOne({
+            userId,
+            roleId,
+            guildId,
+          })
+          .then(async () => {
+            logger.verbose(
+              `Shop role document ${roleId} has been deleted from user ${userId}.`
+            );
+          })
+          .catch(async (error) => {
+            logger.error(
+              `Error deleting shop role document ${roleId} from user ${userId}.`,
+              error
+            );
+          });
         return;
       }
 
@@ -79,23 +113,6 @@ export default async (client: Client) => {
 
           if (!rMember) {
             logger.error(`Member ${userId} not found for shop role ${roleId}.`);
-            await shopRoleSchema
-              .deleteOne({
-                userId,
-                roleId,
-                guildId,
-              })
-              .then(async () => {
-                logger.verbose(
-                  `Shop role document ${roleId} has been deleted from user ${userId}.`
-                );
-              })
-              .catch(async (error) => {
-                logger.error(
-                  `Error deleting shop role document ${roleId} from user ${userId}.`,
-                  error
-                );
-              });
             return;
           }
 
