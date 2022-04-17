5c5859a3e4400566997825957dba38687c1460a8
Vermium Sifell <vermium@zyner.org>
2022-04-17 18:09:59 +0200

    🥅 error handling in event handler

--- src/handlers/events.ts
+++ src/handlers/events.ts
@@ -5,12 +5,14 @@ import logger from "@logger";
 export default async (client: Client) => {
   fs.readdir("./src/events", async (error, events) => {
     if (error) {
-      return logger?.error(`Error reading plugins: ${error}`);
+      return logger.error(`Error reading plugins: ${error}`);
     }
 
     await Promise.all(
-      events?.map(async (eventName) => {
+      events.map(async (eventName) => {
         const event = await import(`../events/${eventName}`);
+
+        logger.verbose(`Loaded event: ${eventName}`);
 
         if (event.once) {
           return client.once(event.default.name, async (...args) =>
@@ -18,12 +20,16 @@ export default async (client: Client) => {
           );
         }
 
-        logger?.verbose(`Loaded event: ${eventName}`);
-
         return client.on(event.default.name, async (...args) =>
           event.default.execute(...args)
         );
       })
-    );
+    )
+      .then(async () => {
+        logger.debug("Successfully loaded events.");
+      })
+      .catch(async (err) => {
+        logger.error(err);
+      });
   });
 };
