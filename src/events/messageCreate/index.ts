3d31ca80a3bc8607d6e3a63803f9269c7fdca2aa
Vermium Sifell <vermium@zyner.org>
2022-04-17 20:47:36 +0200

    🚑 counters crashing bot

--- src/events/messageCreate/index.ts
+++ src/events/messageCreate/index.ts
@@ -4,7 +4,7 @@ import modules from "@events/messageCreate/modules";
 export default {
   name: "messageCreate",
   async execute(message: Message) {
-    await modules.counters.execute(message);
+    await modules.credits.execute(message);
     await modules.points.execute(message);
     await modules.counters.execute(message);
   },
