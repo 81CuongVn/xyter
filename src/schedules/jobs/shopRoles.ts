503c8b5e97fb5bedfe041d76030196f5f6fd5af9
Vermium Sifell <vermium@zyner.org>
2022-04-17 18:13:31 +0200

    🔧 remove shopRole document if user not found

--- src/schedules/jobs/shopRoles.ts
+++ src/schedules/jobs/shopRoles.ts
@@ -79,6 +79,23 @@ export default async (client: Client) => {
 
           if (!rMember) {
             logger.error(`Member ${userId} not found for shop role ${roleId}.`);
+            await shopRoleSchema
+              .deleteOne({
+                userId,
+                roleId,
+                guildId,
+              })
+              .then(async () => {
+                logger.verbose(
+                  `Shop role document ${roleId} has been deleted from user ${userId}.`
+                );
+              })
+              .catch(async (error) => {
+                logger.error(
+                  `Error deleting shop role document ${roleId} from user ${userId}.`,
+                  error
+                );
+              });
             return;
           }
 
