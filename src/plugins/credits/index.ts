4fb241bc9745e3f5e58ca56ff65eac9f8c9e5a58
Vermium Sifell <vermium@zyner.org>
2022-04-22 15:11:39 +0200

    ♻️ credit module handler

--- src/plugins/credits/index.ts
+++ src/plugins/credits/index.ts
@@ -1,44 +1,37 @@
-// Dependencies
 import { SlashCommandBuilder } from "@discordjs/builders";
 import { CommandInteraction } from "discord.js";
-
 import logger from "@logger";
 
-// Modules
-import modules from "@root/plugins/credits/modules";
+import modules from "@plugins/credits/modules";
 
 export default {
-  metadata: { author: "Zyner" },
   data: new SlashCommandBuilder()
     .setName("credits")
     .setDescription("Manage your credits.")
+
     .addSubcommand(modules.balance.data)
     .addSubcommand(modules.gift.data)
     .addSubcommand(modules.top.data)
     .addSubcommand(modules.work.data),
+
   async execute(interaction: CommandInteraction) {
     const { options } = interaction;
 
-    if (options?.getSubcommand() === "balance") {
-      logger?.verbose(`Executing balance subcommand`);
-      return modules.balance.execute(interaction);
+    switch (options.getSubcommand()) {
+      case "balance":
+        await modules.balance.execute(interaction);
+        break;
+      case "gift":
+        await modules.gift.execute(interaction);
+        break;
+      case "top":
+        await modules.top.execute(interaction);
+        break;
+      case "work":
+        await modules.work.execute(interaction);
+        break;
+      default:
+        logger.verbose(`Unknown subcommand ${options.getSubcommand()}`);
     }
-
-    if (options?.getSubcommand() === "gift") {
-      logger?.verbose(`Executing gift subcommand`);
-      return modules.gift.execute(interaction);
-    }
-
-    if (options?.getSubcommand() === "top") {
-      logger?.verbose(`Executing top command`);
-      return modules.top.execute(interaction);
-    }
-
-    if (options?.getSubcommand() === "work") {
-      logger?.verbose(`Executing work command`);
-      return modules.work.execute(interaction);
-    }
-
-    logger?.verbose(`Unknown subcommand ${options?.getSubcommand()}`);
   },
 };
