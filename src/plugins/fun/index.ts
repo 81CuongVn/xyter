b924ab9f3d3a0791387aa2364e02654b4de61e79
Vermium Sifell <vermium@zyner.org>
2022-04-22 16:13:05 +0200

    ✨ add plugin fun and module meme

---
+++ src/plugins/fun/index.ts
@@ -0,0 +1,25 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { CommandInteraction } from "discord.js";
+import logger from "@logger";
+
+import modules from "@plugins/fun/modules";
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("fun")
+    .setDescription("Fun commands.")
+
+    .addSubcommand(modules.meme.data),
+
+  async execute(interaction: CommandInteraction) {
+    const { options } = interaction;
+
+    switch (options.getSubcommand()) {
+      case "meme":
+        await modules.meme.execute(interaction);
+        break;
+      default:
+        logger.verbose(`Unknown subcommand ${options.getSubcommand()}`);
+    }
+  },
+};
