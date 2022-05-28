50f070787c95546a22337d564b54077d1f305980
Vermium Sifell <vermium@zyner.org>
2022-05-28 12:54:37 +0200

    🐛 Default embed if guild is null #337

--- src/helpers/getEmbedConfig.ts
+++ src/helpers/getEmbedConfig.ts
@@ -1,17 +1,19 @@
 import guildSchema from "@schemas/guild";
+import * as embedConfig from "@config/embed";
 
-import { ColorResolvable, Guild } from "discord.js";
+import { Guild } from "discord.js";
 
-export default async (guild: Guild) => {
+export default async (guild: Guild | null) => {
+  if (guild == null)
+    return {
+      ...embedConfig,
+    };
+
   const guildConfig = await guildSchema.findOne({ guildId: guild.id });
 
   if (guildConfig == null)
     return {
-      successColor: "#22bb33" as ColorResolvable,
-      waitColor: "#f0ad4e" as ColorResolvable,
-      errorColor: "#bb2124" as ColorResolvable,
-      footerIcon: "https://github.com/ZynerOrg.png",
-      footerText: "https://github.com/ZynerOrg/xyter",
+      ...embedConfig,
     };
 
   return guildConfig.embeds;
