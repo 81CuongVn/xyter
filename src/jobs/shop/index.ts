f70bbc9171aa096c74b33623d04b55ef08fdd589
Vermium Sifell <vermium@zyner.org>
2022-06-11 19:45:07 +0200

    🧑‍💻 shopRoles job splitted

---
+++ src/jobs/shop/index.ts
@@ -0,0 +1,12 @@
+// Dependencies
+import { Client } from "discord.js";
+
+import * as roles from "./modules/roles";
+
+export const options = {
+  schedule: "*/5 * * * * *", // https://crontab.guru/
+};
+
+export const execute = async (client: Client) => {
+  await roles.execute(client);
+};
