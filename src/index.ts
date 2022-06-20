805bd5c4aa5fecd3383fabb7147a5dba9c85298e
Vermium Sifell <vermium@zyner.org>
2022-06-20 13:24:13 +0200

    ⚰️ tsconfig-paths not in use anymore

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import "tsconfig-paths/register"; // Allows using tsconfig.json paths during runtime
-
 import { token, intents } from "./config/discord";
 
 import { Client } from "discord.js"; // discord.js
