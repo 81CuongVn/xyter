023589f9d093818a38815635a7135aa75d45ec6e
Vermium Sifell <vermium@zyner.org>
2022-06-20 13:24:56 +0200

    🚚 fixed ICommand path in discord.d.ts

--- src/types/common/discord.d.ts
+++ src/types/common/discord.d.ts
@@ -1,5 +1,5 @@
 import { Collection } from "discord.js";
-import ICommand from "../interfaces/Command";
+import ICommand from "../../interfaces/Command";
 
 declare module "discord.js" {
   export interface Client extends DJSClient {
